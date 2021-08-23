2d8b4e01d8de971bcad67f951641a4cdeb4b66af
unknown <edgar.adirga@gmail.com>
2021-08-23 14:53:16 +0700

    myrequest routing

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,11 @@
 const express = require('express')
 const router = express.Router()
-// const UserC = require('../controllers/user.js')
-// const productC = require('../controllers/product')
-// const tagC = require('../controllers/tags')
-// const cartC = require('../controllers/cart-controller')
 const UserC = require('../controllers/userController')
 const CategoryC = require('../controllers/categoryController')
 const RequestC = require('../controllers/requestController')
 const categoryRoute = require('./categories')
 const requestRoute = require('./requests')
 const {authentication, adminAuth, authorization} = require('../middlewares/auth')
-// const checkStock = require('../middlewares/checkStock')
 
 // user
 router.post('/login', authentication, adminAuth, UserC.login)
--- routes/requests.js
+++ routes/requests.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const RequestC = require('../controllers/requestController')
 const {authentication, adminAuth, authorization} = require('../middlewares/auth')
 
+router.get('/myrequests', authentication, RequestC.getMyRequest)
 router.get('/', RequestC.getRequest)
 router.get('/:id', function(req, res, next) {
     if(req.headers.access_token) {authentication(req, res, next)}
@@ -11,6 +12,5 @@ router.get('/:id', function(req, res, next) {
 router.post('/', authentication, adminAuth, RequestC.postRequest)
 router.put('/:id', authentication, adminAuth, RequestC.putRequest)
 router.delete('/:id', authentication, adminAuth, RequestC.delRequest)
-
 
 module.exports = router
